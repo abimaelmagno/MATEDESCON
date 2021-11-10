7798c75d0b6098ab1ce7a07750f07e308de7f921
Leonardo Batista <leopbatista@yahoo.com.br>
2021-11-10 10:08:20 -0300

    Página de status - descartar

--- app/controllers/lancamentos_controller.rb
+++ app/controllers/lancamentos_controller.rb
@@ -1,4 +1,13 @@
 class LancamentosController < ApplicationController
 
+def compra
+  @firma.product = @firma.product + @compra.quantity
+  @firma.capital = @firma.capital - @compra.valor
+end
+
+def venda
+  @firma.product = @firma.product - @venda.quantity
+  @firma.capital = @firma.capital + @venda.valor
+end
 
 end
--- app/models/lancamento.rb
+++ app/models/lancamento.rb
@@ -2,9 +2,5 @@ class Lancamento < ApplicationRecord
   belongs_to :firma
   has_many :firmas
 
-  def capital
-  end
-
-  def estoque
-  end
+  
 end
