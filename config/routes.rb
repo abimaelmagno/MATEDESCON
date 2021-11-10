7987ab8d38a3d3694c06acedc967d681da562dc2
eduardo pimenta <ed83isme@gmail.com>
2021-11-10 00:13:57 -0300

    added a bank page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
-  resources :firmas, except: [:edit]
+  resources :firmas, except: [:edit] do
+    resources :banks, only: [:index, :show]
+  end
 
 end
