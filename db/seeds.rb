7467f737e36fe4bd1413568b92e2ffde5d69329d
Leonardo Batista <leopbatista@yahoo.com.br>
2021-11-10 14:36:17 -0300

    Página de status - descartar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing users database..."
+User.destroy_all
+puts "Products database has been successfully cleared."
+puts "Clearing users database..."
+Firma.destroy_all
+puts "Products database has been successfully cleared."
+puts "Clearning fornecedores database..."
+Fornecedor.destroy_all
+puts "Fornecedoress database has been successfully cleared."
+puts "Seeding Users database..."
+5.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    course: Faker::Educator.course_name,
+    period: rand(1..10),
+    email: Faker::Internet.email,
+    password: "123123"
+  )
+end
+puts "10 users have been created."
+puts "Seeding Firmas database..."
+10.times do
+  Firma.create!(
+    name: Faker::Company.name,
+    sector: ["Vestuário", "Alimentação", "brinquedo"].sample,
+    user_id: User.all.sample.id
+  )
+end
+puts "15 firmass have been created."
+puts "Seeding Fornecedores database..."
+100.times do
+  Fornecedor.create!(
+    name: Faker::Company.industry,
+    name_produto: ["roupa", "quentinha", "brinquedo"].sample,
+    preco: rand(50..100),
+    estoque: rand(100..1000),
+    prazo: Faker::Date.in_date_period,
+    firma_id: Firma.all.sample.id
+  )
+end
+puts "30 products have been successfully created."
