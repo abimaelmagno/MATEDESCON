90a6df5d112e70ba4bc12977c7f9db128a1902a0
HugoHubner <hugo.hubner@rfb.gov.br>
2021-11-12 12:55:17 -0300

    sexta manha 02

--- db/schema.rb
+++ db/schema.rb
@@ -65,18 +65,6 @@ ActiveRecord::Schema.define(version: 2021_11_11_234543) do
     t.index ["user_id"], name: "index_firmas_on_user_id"
   end
 
-  create_table "fonecedors", force: :cascade do |t|
-    t.string "nome_empresa"
-    t.string "nome_produto"
-    t.integer "estoque"
-    t.integer "price"
-    t.date "prazo_pagamento"
-    t.bigint "firma_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["firma_id"], name: "index_fonecedors_on_firma_id"
-  end
-
   create_table "fornecedors", force: :cascade do |t|
     t.string "name"
     t.string "name_produto"
@@ -124,7 +112,6 @@ ActiveRecord::Schema.define(version: 2021_11_11_234543) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "banks", "firmas"
   add_foreign_key "firmas", "users"
-  add_foreign_key "fonecedors", "firmas"
   add_foreign_key "fornecedors", "firmas"
   add_foreign_key "lancamentos", "firmas"
 end
